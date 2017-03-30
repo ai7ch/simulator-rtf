748f1b5764048dc5228ef7dc3b059316732be1da
Aitch <dev-aitch@hotmail.com>
2017-03-30 09:48:16 +0200

    pull init

---
+++ modules/modules.php
@@ -0,0 +1,19 @@
+<?php 
+
+	
+	/**
+	* 
+	*/
+	class Modules
+	{
+		
+		function __construct($connect)
+		{
+			//connect to db
+		}
+
+		function __destruct($deconnect)
+		{
+			//deconnect from db
+		}
+	}
