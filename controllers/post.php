54943a23c89bd499162189f61f9d4708ca931885
Aitch <dev-aitch@hotmail.com>
2017-04-03 11:22:43 +0200

    check if form was posted correctly

--- controllers/post.php
+++ controllers/post.php
@@ -30,11 +30,11 @@
 
 
 	echo "<pre>";
-	if(isset($_POST)){
+	if(isset($_POST['submit'])){
 		echo "yes POSTED";
 	}
 	echo "<hr>";
-	$fields = $_GET;
+	$fields = $_POST;
 	$patterns = [
 		'taux' => '^taux',
 		'proprietes' => '[-]\d',
@@ -47,37 +47,8 @@
 	$proprietes = groupFields($patterns['proprietes'], $fields);
 	print_r($proprietes);
 
-	/****************************************/	
-
-	$arr_main_array = array('foo-test' => 123, 'other-test' => 456, 'foo-result' => 789);
-
-	foreach($arr_main_array as $key => $value){
-	    $exp_key = explode('-', $key);
-	    if($exp_key[0] == 'foo'){
-	         $arr_result[] = $value;
-	    }
-	}
-
-	if(isset($arr_result)){
-	    //print_r($arr_result);
-	}
-
-	/***************************************/
-
-
-	echo "<br>";
-	
-	foreach ($proprietes as $key => $value) { 
-		$id = explode('-', substr($key, -1));
-		print_r($id);
-	}
-
-	//print_r($total_proprietes);
-
-	echo '<br>';
-
-	$propriete_unique = array_unique($proprietes);
-	//print_r($propriete_unique);
+	echo "<br><hr><br>";
+	print_r($fields);
 
 
 echo "</pre>";
